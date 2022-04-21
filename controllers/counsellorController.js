78c1add1da9c228080fd2636f026e7a2ce9429ea
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-04-21 21:36:14 +0530

    ...

--- controllers/counsellorController.js
+++ controllers/counsellorController.js
@@ -1,7 +1,33 @@
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const Counsellor = require("../models/counsellor");
 const StudentMails = require("../models/studentMails");
+const { catchAsync } = require("../Utils/ErrorHandling");
 
-exports.signup = async (req, res) => {};
+exports.signin = (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: info ? info.message : "Login failed",
+        user: user,
+      });
+    }
+    req.login(user, { session: false }, async (err) => {
+      if (err) throw err;
+      const token = jwt.sign(
+        { id: user._id.toJSON(), role: user.role },
+        process.env.SECRET,
+        {
+          expiresIn: 604800,
+        }
+      );
+      res.status(200).json({
+        message: info.message,
+        token: token,
+        user: user,
+      });
+    });
+  })(req, res, next);
+};
