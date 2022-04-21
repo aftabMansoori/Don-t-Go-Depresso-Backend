7b14d2861a0e2ff24a166fdf69cc55472328f194
aftabMansoori <mohammed.aftab18@siesgst.ac.in>
2022-04-21 21:16:27 +0530

    updated the websocket connection

--- controllers/videoCallController.js
+++ controllers/videoCallController.js
@@ -1,21 +1,48 @@
 module.exports.videoCallSetup = (io) => {
-  io.on("connection", (socket) => {
-    socket.emit("me", socket.id);
+  // io.on("connection", (socket) => {
+  //   socket.emit("me", socket.id);
 
+  //   socket.on("disconnect", () => {
+  //     socket.broadcast.emit("callEnded");
+  //   });
+
+  //   socket.on("callUser", (data) => {
+  //     io.to(data.userToCall).emit("callUser", {
+  //       signal: data.signalData,
+  //       from: data.from,
+  //       name: data.name,
+  //     });
+  //   });
+
+  //   socket.on("answerCall", (data) => {
+  //     io.to(data.to).emit("callAccepted", data.signal);
+  //   });
+  // });
+  io.on("connection", function (socket) {
+    socket.on("join", function (data) {
+      socket.join(data.roomId);
+      socket.room = data.roomId;
+      const sockets = io.of("/").in().adapter.rooms[data.roomId];
+      if (sockets.length === 1) {
+        socket.emit("init");
+      } else {
+        if (sockets.length === 2) {
+          io.to(data.roomId).emit("ready");
+        } else {
+          socket.room = null;
+          socket.leave(data.roomId);
+          socket.emit("full");
+        }
+      }
+    });
+    socket.on("signal", (data) => {
+      io.to(data.room).emit("desc", data.desc);
+    });
     socket.on("disconnect", () => {
-      socket.broadcast.emit("callEnded");
-    });
-
-    socket.on("callUser", (data) => {
-      io.to(data.userToCall).emit("callUser", {
-        signal: data.signalData,
-        from: data.from,
-        name: data.name,
-      });
-    });
-
-    socket.on("answerCall", (data) => {
-      io.to(data.to).emit("callAccepted", data.signal);
+      const roomId = Object.keys(socket.adapter.rooms)[0];
+      if (socket.room) {
+        io.to(socket.room).emit("disconnected");
+      }
     });
   });
 };
